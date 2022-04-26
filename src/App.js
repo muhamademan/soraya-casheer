bbd8d386436598856769c18b31f1f0fbf5ab2319
Muhamad Eman Sulaeman <37392560+muhamademan@users.noreply.github.com>
2022-04-26 09:58:28 +0700

    add library at component Hasil, ListCategories and NavbarComponent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import NavbarComponent from './component/NavbarComponent';
+import { Col, Row, Container } from 'react-bootstrap';
+import ListCategories from './component/ListCategories';
+import Hasil from './component/Hasil';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+      <div className='mt-3'>
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4><strong>Daftar Product</strong></h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
