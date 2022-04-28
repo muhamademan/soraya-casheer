a74650e885e328ea83e34ef6df2c6a1c9e42c228
Muhamad Eman Sulaeman <37392560+muhamademan@users.noreply.github.com>
2022-04-28 12:53:57 +0700

    update App functional component to class component, create componentDidMount() with API_URL + products

--- src/App.js
+++ src/App.js
@@ -1,29 +1,92 @@
-import logo from './logo.svg';
-// import './App.css';
 import NavbarComponent from './component/NavbarComponent';
 import { Col, Row, Container } from 'react-bootstrap';
 import ListCategories from './component/ListCategories';
 import Hasil from './component/Hasil';
+import React, { Component, Fragment } from 'react'
+import { API_URL } from './utils/Costants'
+import axios from 'axios';
+import Menus from './component/Menus';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className='mt-3'>
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4><strong>Daftar Produk</strong></h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
-      </div>
-    </div>
-  );
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     menus: []
+  //   }
+  // }
+
+  state = {
+    menus: []
+  }
+
+  componentDidMount() {
+    axios.get(API_URL + "products")
+      .then((res) => {
+        this.setState({
+          menus: res.data
+        })
+      })
+      .catch(error => {
+        console.log('Error : ', error);
+      })
+  }
+
+  render() {
+
+    const { menus } = this.state
+
+    return (
+      <Fragment>
+        <div className="App">
+          <NavbarComponent />
+          <div className='mt-3'>
+            <Container fluid>
+              <Row>
+                <ListCategories />
+                <Col>
+                  <h4><strong>Daftar Produk</strong></h4>
+                  <hr />
+                  <Row>
+                    {
+                      menus && menus.map((menu) => {
+                        return <Menus
+                          key={menu.id}
+                          data={menu} />
+                      })
+                    }
+                  </Row>
+                </Col>
+                <Hasil />
+              </Row>
+            </Container>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <NavbarComponent />
+//       <div className='mt-3'>
+//         <Container fluid>
+//           <Row>
+//             <ListCategories />
+//             <Col>
+//               <h4><strong>Daftar Produk</strong></h4>
+//               <hr />
+//             </Col>
+//             <Hasil />
+//           </Row>
+//         </Container>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
