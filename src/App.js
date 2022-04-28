cfdb13623697b6587b6fe302a389bf19cd224e7a
Muhamad Eman Sulaeman <37392560+muhamademan@users.noreply.github.com>
2022-04-28 13:02:07 +0700

    delete old App functional component

--- src/App.js
+++ src/App.js
@@ -67,26 +67,4 @@ class App extends Component {
   }
 }
 
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavbarComponent />
-//       <div className='mt-3'>
-//         <Container fluid>
-//           <Row>
-//             <ListCategories />
-//             <Col>
-//               <h4><strong>Daftar Produk</strong></h4>
-//               <hr />
-//             </Col>
-//             <Hasil />
-//           </Row>
-//         </Container>
-//       </div>
-//     </div>
-//   );
-// }
-
 export default App;
